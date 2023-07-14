fcd52c858cd7c4f0a8f8d917807265cc59666fd4
Divya Dattatray Gadekar <116143709+divya-gadekar28@users.noreply.github.com>
2023-07-14 11:59:00 +0530

    prog2.py

---
+++ prog2.py
@@ -0,0 +1,7 @@
+#Q.2)	Write a Python program to find set difference, union, intersection and symmetric difference.
+A = {0, 2, 4, 6, 8};
+B = {1, 2, 3, 4, 5};
+print("Union :", A | B)
+print("Intersection :", A & B)
+print("Difference :", A - B)
+print("Symmetric difference :", A ^ B)
