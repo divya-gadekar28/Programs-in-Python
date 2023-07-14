2286151498504ebb4de5dcc9acc86c0d04df9fd5
Divya Dattatray Gadekar <116143709+divya-gadekar28@users.noreply.github.com>
2023-07-14 11:59:41 +0530

    prog3.py

---
+++ prog3.py
@@ -0,0 +1,6 @@
+#Q.1)	Write a Python program to get the 4th element from front and 4th element from last of a tuple.
+t = ("w", 3, "r", "e", "s", "o", "u", "r", "c", "e")
+i = t[3]
+print("4th Elements From first in given tuple :",i)
+j = t[-4]
+print("4th Elements From Last in given Tuple :",j)
